5b068248b147c8744f5607308ed4642c713ce25e
SPIROS\spiros <uberi12345@hotmail.com>
2018-01-11 19:09:31 +0200

    admin.php

--- admin.php
+++ admin.php
@@ -1,57 +1,7 @@
 <?php session_start(); ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <title>Bootstrap Example</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 
-</head>
-<body>
-
-<nav class="navbar navbar-inverse">
-  <div class="container-fluid">
-    <div class="navbar-header">
-      <a class="navbar-brand" href="#">Lessons</a>
-    </div>
-    <ul class="nav navbar-nav">
-      <li class="active"><a href="home.html">Home</a></li>
-      <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">Μαθήματα <span class="caret"></span></a>
-        <ul class="dropdown-menu">
-          <li><a href="#">Εισαγωγή μαθημάτων</a></li>
-          <li><a href="#">Ενημέρωση μαθημάτων</a></li>
-          <li><a href="#">Διαγραφή μαθημάτων</a></li>
-        </ul>
-      </li>
-      <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">Ερωτήσεις Μαθημάτων<span class="caret"></span></a>
-        <ul class="dropdown-menu">
-          <li><a href="#">Δημιουργία ερωτήσεων</a></li>
-          <li><a href="#">Ενημέρωση ερωτήσεων</a></li>
-          <li><a href="#">Διαγραφή ερωτήσεων</a></li>
-        </ul>
-      </li>
-      <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">Χρήστες<span class="caret"></span></a>
-        <ul class="dropdown-menu">
-          <li><a href="#">Δημιουργία χρηστών</a></li>
-          <li><a href="#">Ενημέρωση χρηστών</a></li>
-          <li><a href="#">Διαγραφή χρηστών</a></li>
-        </ul>
-      </li>
-    </ul>
-    <ul class="nav navbar-nav navbar-right">
-      <li><a href="./logout.php"><span class="glyphicon glyphicon-log-out"></span>Log_out</a></li>
-    </ul>
-  </div>
-</nav>
-
+<?php  require "nav_admin.html";?>
 <div class="container">
   <h1>HELLO <?php echo $_SESSION['username']; ?> </h1>
   <p>The .navbar-right class is used to right-align navigation bar buttons.</p>
 </div>
-
-
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-</body>
-</html>
