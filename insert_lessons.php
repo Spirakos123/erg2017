e024a5d40c3662f163e2eed443fec3b753628fe5
SPIROS\spiros <uberi12345@hotmail.com>
2018-01-11 21:51:05 +0200

    fixed

--- insert_lessons.php
+++ insert_lessons.php
@@ -4,10 +4,7 @@ $title = $_POST['title'];
 // $kenou =$_POST['kenou'];
 // $checkbox = $_POST['checkbox'];
 // $pollaplis =$_POST['pollaplis'];
-echo "eeeee";
-echo $title;
 
-//require "nav_admin.html";
  $con = mysqli_connect($local,$root,$pass,$idm);
   if (!$con) {
   die('Could not connect: ' . mysql_error());
@@ -16,7 +13,7 @@ echo $title;
   // $sql= "INSERT INTO users (firstname, lastname, email,username,password)
   // VALUES ('NULL', $firstname, $lastname,$email,$user_name,$password)";
 
-$sql="INSERT INTO lessons ('title')VALUES( '$title' )";
+$sql="INSERT INTO lessons (title) VALUES('$title')";
   if (mysqli_query($con, $sql)) {
     echo "New record created successfully";
   }else{
